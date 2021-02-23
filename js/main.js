6ff72341f7c4f93470e3701b233b51a116dce1a1
Adriana <zinkoadriana@gmail.com>
2021-02-23 23:37:25 +0200

    Eslint and Prettier

--- js/main.js
+++ js/main.js
@@ -1,70 +1,70 @@
-'use strict'
-const input = document.getElementById("input");
-    const addTodoButton = document.getElementById("addTodoButton");
-    const todoUL = document.getElementById("todoUL");
-    const filterOptions = document.querySelector(".filter-todos");
-
-    addTodoButton.addEventListener("click", addTodo);
-    todoUL.addEventListener("click", remove);
-    filterOptions.addEventListener("change", filterTodos);
-
-    // Add todo
-    function addTodo(e) {
-      e.preventDefault();
-      const todoText = input.value;
-      const todoEl = `<li class="item"><p class="text">${todoText}</p>
+const input = document.getElementById('input');
+const addTodoButton = document.getElementById('addTodoButton');
+const todoUL = document.getElementById('todoUL');
+const filterOptions = document.querySelector('.filter-todos');
+// Add todo
+function addTodo(e) {
+  e.preventDefault();
+  const todoText = input.value;
+  const todoEl = `<li class="item"><p class="text">${todoText}</p>
       <button class="delete button" id="deleteTodoButton"><i class="far fa-trash-alt"></i>Delete</button>
       <button class="complete button" id="completeTodoButton"><i class="fas fa-check"></i>Completed</button>   
       </li>`;
-      input.value = "";
-      input.focus();
+  input.value = '';
+  input.focus();
 
-      if (!todoText) {
-        alert("You must type a todo");
-      } else {
-        todoUL.insertAdjacentHTML("beforeend", todoEl);
-      }
-    }
+  if (!todoText) {
+    alert('You must type a todo');
+  } else {
+    todoUL.insertAdjacentHTML('beforeend', todoEl);
+  }
+}
 
-    // Remove/Complete todo
-    function remove(e) {
-      if (e.target.id == "deleteTodoButton") {
-        e.target.parentElement.remove();
-        input.focus();
-      } else if (e.target.id == "completeTodoButton") {
-        e.target.previousElementSibling.previousElementSibling.classList.toggle(
-          "completed"
-        );
-        input.focus();
-      }
-    }
+// Remove/Complete todo
+function remove(e) {
+  if (e.target.id === 'deleteTodoButton') {
+    e.target.parentElement.remove();
+    input.focus();
+  } else if (e.target.id === 'completeTodoButton') {
+    e.target.previousElementSibling.previousElementSibling.classList.toggle(
+      'completed'
+    );
+    input.focus();
+  }
+}
 
-    function filterTodos(e) {
-      const todos = todoUL.childNodes;
-      todos.forEach(function(todoEl) {
-        if (todoEl.nodeName === "LI") {
-          switch (e.target.value) {
-            case "all":
-              todoEl.style.display = "flex";
-              break;
+function filterTodos(e) {
+  const todos = todoUL.childNodes;
+  todos.forEach((todoEl) => {
+    if (todoEl.nodeName === 'LI') {
+      const todoElem = todoEl;
+      switch (e.target.value) {
+        case 'all':
+          todoElem.style.display = 'flex';
+          break;
 
-            case "completed":
-              if (todoEl.children[0].classList.contains("completed")) {
-                todoEl.style.display = "flex";
-              } else {
-                todoEl.style.display = "none";
-              }
-              break;
-
-            case "uncompleted":
-              if (todoEl.children[0].classList.contains("completed")) {
-                todoEl.style.display = "none";
-              } else {
-                todoEl.style.display = "flex";
-              }
-              break;
+        case 'completed':
+          if (todoElem.children[0].classList.contains('completed')) {
+            todoElem.style.display = 'flex';
+          } else {
+            todoElem.style.display = 'none';
           }
-        }
-      });
+          break;
+
+        case 'uncompleted':
+          if (todoElem.children[0].classList.contains('completed')) {
+            todoElem.style.display = 'none';
+          } else {
+            todoElem.style.display = 'flex';
+          }
+          break;
+        default:
+          break;
+      }
     }
- 
+  });
+}
+
+addTodoButton.addEventListener('click', addTodo);
+todoUL.addEventListener('click', remove);
+filterOptions.addEventListener('change', filterTodos);
